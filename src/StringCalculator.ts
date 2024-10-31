7b77918ff9fe24491c602900b7a340fde0cf112f
Ashok Pal <88579810+Ashokpal24@users.noreply.github.com>
2024-10-31 03:50:29 +0000

    Two numbers addition, comma delimiter.

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -2,6 +2,7 @@ export class StringCalculator {
   add(numbers: string): number {
     if (!numbers) return 0;
     if (numbers.length == 1 && !isNaN(Number(numbers))) return Number(numbers);
-    return 0;
+    const numArray = numbers.split(",").map(Number);
+    return numArray.reduce((sum, num) => sum + num, 0);
   }
 }
--- src/test/StringCalculator.test.ts
+++ src/test/StringCalculator.test.ts
@@ -11,4 +11,7 @@ describe("StringCalculator", () => {
   test("returns the number itself for a single number", () => {
     expect(calc.add("5")).toBe(5);
   });
+  test("returns the sum of two numbers", () => {
+    expect(calc.add("1,2")).toBe(3);
+  });
 });
