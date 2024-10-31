c8c5afdf09573bba9cad241c99e49a88721159db
Ashok Pal <88579810+Ashokpal24@users.noreply.github.com>
2024-10-31 03:46:50 +0000

    Single number returns itself.

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,5 +1,7 @@
 export class StringCalculator {
   add(numbers: string): number {
+    if (!numbers) return 0;
+    if (numbers.length == 1 && !isNaN(Number(numbers))) return Number(numbers);
     return 0;
   }
 }
--- src/test/StringCalculator.test.ts
+++ src/test/StringCalculator.test.ts
@@ -8,4 +8,7 @@ describe("StringCalculator", () => {
   test("returns 0 for an empty string", () => {
     expect(calc.add("")).toBe(0);
   });
+  test("returns the number itself for a single number", () => {
+    expect(calc.add("5")).toBe(5);
+  });
 });
